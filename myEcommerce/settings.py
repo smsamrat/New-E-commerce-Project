29985f3c16d1378180aaf415157ea57558b1e1df
you <97931972+smsamrat@users.noreply.github.com>
2022-04-27 22:25:51 +0600

    modify django crispy form

--- myEcommerce/settings.py
+++ myEcommerce/settings.py
@@ -42,8 +42,13 @@ INSTALLED_APPS = [
     'App_store',
     'App_order',
     'App_payment',
-
+    "crispy_forms",
+    "crispy_bootstrap5",
+    
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 AUTH_USER_MODEL = 'App_account.User'
 MIDDLEWARE = [
